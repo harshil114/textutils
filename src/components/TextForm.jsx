46003571c73dd371f77da6b74e74980b5f7eac13
harshil114 <harshilvispute114@gmail.com>
2024-06-04 19:23:53 +0530

    Add toUpperCase logic to TextForm.jsx

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 export default function TextForm(props){
+
+    const [text,setText] = useState('')
+
+    const handleOnChange = (event) => {
+        setText(event.target.value)
+    }
+
+    const toUpCase = () => {
+        let newText = text.toUpperCase();
+        setText(newText);
+        
+    }
     return(
         <>
             <div className="container">
                 <div className="mb-3">
                     <h1 className="my-3">{props.heading}</h1>                
-                    <textarea className="form-control" id="myBox" rows="8"></textarea>
-                    <button className="btn btn-primary my-3 mx-2">Uppercase</button>
+                    <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="8"></textarea>
+                    <button className="btn btn-primary my-3 mx-2" onClick={toUpCase}>Uppercase</button>
                     <button className="btn btn-primary my-3 mx-2">Lower case</button>
                 </div>
 
@@ -14,9 +28,9 @@ export default function TextForm(props){
                     <p>words and characters</p>
                     <p>Minutes read</p>
                     <h5>Preview</h5>
-                    <p></p>
+                    <p>{text}</p>
                 </div>
-                
+
             </div>
         </>
     );
