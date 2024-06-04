1dad28bc65489e3b56ac3630be66d8eeb363b066
harshil114 <harshilvispute114@gmail.com>
2024-06-04 19:43:54 +0530

    add toLowerCase, wordCount, charCount, readTime logic

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -2,17 +2,25 @@ import { useState } from "react";
 
 export default function TextForm(props){
 
-    const [text,setText] = useState('')
+    const [text,setText] = useState('');
+
 
     const handleOnChange = (event) => {
         setText(event.target.value)
     }
 
     const toUpCase = () => {
-        let newText = text.toUpperCase();
-        setText(newText);
-        
+        setText(text.toUpperCase())
     }
+
+    const toLowerCase = () => {
+        setText(text.toLowerCase())
+    }
+
+    const wordCount = text.split(" ").length;
+    const charCount = text.length;
+    const readTime = 0.008 * wordCount;
+   
     return(
         <>
             <div className="container">
@@ -20,13 +28,13 @@ export default function TextForm(props){
                     <h1 className="my-3">{props.heading}</h1>                
                     <textarea className="form-control" id="myBox" value={text} onChange={handleOnChange} rows="8"></textarea>
                     <button className="btn btn-primary my-3 mx-2" onClick={toUpCase}>Uppercase</button>
-                    <button className="btn btn-primary my-3 mx-2">Lower case</button>
+                    <button className="btn btn-primary my-3 mx-2" onClick={toLowerCase}>Lower case</button>
                 </div>
 
                 <div className="container">
                     <h4>Your text summary</h4>
-                    <p>words and characters</p>
-                    <p>Minutes read</p>
+                    <p>{wordCount} words and {charCount} characters</p>
+                    <p>{readTime.toFixed(2)} Minutes read</p>
                     <h5>Preview</h5>
                     <p>{text}</p>
                 </div>
